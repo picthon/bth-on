47e46dc65f91a442ad031001a62f7bc5fc37593e
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-04 00:47:42 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,12 +3,12 @@ from sample_config import Config
 
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي وضع اكوادك
-    APP_ID = 13740761
-    API_HASH = "4ce319a92c01fab2b02551af8d7f73a4"
-    ALIVE_NAME = "✗ ¦ ↱𝐺𝑜𝑙 𝐷. 𝑅𝑜𝑔𝑒𝑟↲ ¦ ✗"
-    DB_URI = "https://t.me/ZQ_LO"
-    STRING_SESSION = "1ApWapzMBu6PHd8_9XH-xrPwgCDShDMAbztT5fSvlszwzfcGVjU8AI06USKXMdCXYeUzxy-PWnE-ImXGaslv5oUhNcr2znhXJsAfrg9aOV_d65RG4Fah7V5GMcVOTa9frjVkeOSNh79slJpKIQ_fQHyEL1YIW7piOlvOyjfqhohp5Ry1mkrCYEkClzRiBEgwscC5GKmnclD0v4t0KXzS6DqX79InXzBl2QJTi623wV4ehjhst6IofnNeNPIht-2Tq4dVSgNtTQifBORklhxV8za4JYGf0_DYp7fOcGLx_A_lHsnmvMCRUSiusLGEcWGokEz43TpEp3_IVMqtL4iA_gi3djdbI8mw="
-    TG_BOT_TOKEN = "5442362948:AAHs-6Pv1bjclTXA98QGVI2Wmj8caJr4Ouk"
+    APP_ID = 6
+    API_HASH = "ضع كود الايبي هاش"
+    ALIVE_NAME = "اسم حسابك التلي"
+    DB_URI = "رابـط التخـزين الخـاص بك"
+    STRING_SESSION = "كود تيرمــكس"
+    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
     PRIVATE_GROUP_BOT_API_ID = -100
     COMMAND_HAND_LER = "."
     SUDO_COMMAND_HAND_LER = "،"
