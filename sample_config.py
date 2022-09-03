e137d8c2eae433b67ee446bb3d49d19b386907c3
روجر <104784913+rogerpq@users.noreply.github.com>
2022-09-04 00:34:28 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Zed-Thon/Zelzal"
+        "UPSTREAM_REPO", "https://github.com/rogerpq/Repthon-"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
@@ -86,7 +86,7 @@ class Config(object):
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "ZThon")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
-        "THUMB_IMAGE", "https://telegra.ph/file/c91c09fb188f0f281e628.jpg"
+        "THUMB_IMAGE", "https://telegra.ph/file/2be69528a8ae9eb43fc40.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in userbot
     NO_LOAD = list(os.environ.get("NO_LOAD", "").split())
