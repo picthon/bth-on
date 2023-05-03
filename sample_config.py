da880f760cb47ee4753abdb1d15aa46e885730e8
روجر <104784913+rogerpq@users.noreply.github.com>
2023-05-03 15:40:55 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/rogerpq/Repthon-"
+        "UPSTREAM_REPO", "https://github.com/RepthonArabic/RepthonAr"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
